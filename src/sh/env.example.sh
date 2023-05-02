59ce224aa8b0cab7fa55ce4577bd0d65ba15de6a
HDTran91 <khoaiga0103@gmail.com>
2023-05-02 18:00:05 -0500

    connect and create mongodb

---
+++ src/sh/env.example.sh
@@ -0,0 +1,12 @@
+#config database environment variable
+
+export DB_CONNECTION=***
+export DB_HOST=***
+export DB_PORT=***
+export DB_NAME=***
+export DB_USERNAME=***
+export DB_PASSWORD=***
+
+#config app environment vảiable
+export APP_HOST=***
+export APP_PORT=***
