18ee5f4c48fd141755b6bba5d369ef1e523f0db0
HDTran91 <khoaiga0103@gmail.com>
2023-06-26 12:21:52 -0500

    [1.0.0] Register user part 01

---
+++ lang/vi.js
@@ -0,0 +1,6 @@
+export const transValidation ={
+    email_incorrect: "Email phai co dang khoaiga@gmail.com",
+    gender_incorrect: "tai sao gioi tinh lai bi sai",
+    password_incorrect: "Mat khau chua it nhat 8 ki tu bao gom chu hoa, chu thuong va chu so",
+    password_confirmation_incorrect: "nhap khau chua trung khop"
+}
