df395fd564bcc3d1ce669ccb396ae576ecbc1a53
HDTran91 <khoaiga0103@gmail.com>
2023-09-18 15:25:36 -0500

    update_password

--- src/validation/userValidation.js
+++ src/validation/userValidation.js
@@ -17,6 +17,20 @@ let updateInfo =[
         .matches(/^(0)[0-9]{9,10}$/)
 ];
 
+let UpdatePassword = [
+    check("currentPassword", transValidation.password_incorrect)
+        .islength ({min:8})
+        .matches(/^[\s0-9a-zA-Z_ÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚĂĐĨŨƠàáâãèéêìíòóôõùúăđĩũơƯĂẠẢẤẦẨẪẬẮẰẲẴẶẸẺẼỀỀỂưăạảấầẩẫậắằẳẵặẹẻẽềềểỄỆỈỊỌỎỐỒỔỖỘỚỜỞỠỢỤỦỨỪễệỉịọỏốồổỗộớờởỡợụủứừỬỮỰỲỴÝỶỸửữựỳỵỷỹ ]+$/),
+    check("newPassword", transValidation.password_incorrect)
+        .islength ({min:8})
+        .matches(/^[\s0-9a-zA-Z_ÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚĂĐĨŨƠàáâãèéêìíòóôõùúăđĩũơƯĂẠẢẤẦẨẪẬẮẰẲẴẶẸẺẼỀỀỂưăạảấầẩẫậắằẳẵặẹẻẽềềểỄỆỈỊỌỎỐỒỔỖỘỚỜỞỠỢỤỦỨỪễệỉịọỏốồổỗộớờởỡợụủứừỬỮỰỲỴÝỶỸửữựỳỵỷỹ ]+$/),
+    check("confirmNewPassword", transValidation.password_confirmation_incorrect)
+        .custom((value,{req})=> {
+            return value === req.body.newPassword
+        })
+]
+
 module.exports ={
-    updateInfo: updateInfo
+    updateInfo: updateInfo,
+    UpdatePassword :UpdatePassword 
 }
